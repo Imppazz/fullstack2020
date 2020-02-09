b928ab0d5a64ae92b4e66e2041c6dd8e2713188f
DESKTOP-D1VLN43\Ilmari <imppa.kolehmainen@gmail.com>
2020-02-09 20:31:17 +0200

    3.11

---
+++ osa3_puhelinluettelo/build/precache-manifest.24e561768a4102ef5112467e67eb5dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494963e3324f2ba28209c8b3697f0d7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6f3345af4835f8550d",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6f3345af4835f8550d",
+    "url": "/static/js/main.13dd31d4.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
---
+++ osa3_puhelinluettelo/build/precache-manifest.f8174b71397795248fe4d1505da617fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe35d002f6e0717d1faf0863523cd626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a340368250de37033f9d",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a340368250de37033f9d",
+    "url": "/static/js/main.db1a6a18.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
