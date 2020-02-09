13175daa8f2096b04a0c1ab8eafad79191c5188e
DESKTOP-D1VLN43\Ilmari <imppa.kolehmainen@gmail.com>
2020-02-09 15:24:15 +0200

    Osa 3

---
+++ osa3_puhelinluettelo/index.js
@@ -0,0 +1,109 @@
+const express = require('express')
+var morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('person', function (req) { return JSON.stringify(req.body) })
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.person(req)
+  ].join(' ')
+}, {
+  skip: function (req, res) { return req.method !== 'POST' }
+}))
+
+let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: 1
+    },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: 2
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: 3
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.post('/api/persons', (req, res) => {
+    const body = req.body
+  
+    if (!body.name) {
+      return res.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+    if (!body.number) {
+      return res.status(400).json({ 
+        error: 'number missing' 
+      })
+    }
+    if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+      return res.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: Math.floor(Math.random()*1000000)
+    }
+  
+    persons = persons.concat(person)
+    res.json(person)
+  })
+
+  app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> <p> ${Date()} </p>`)
+  })
+
+  app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(note => note.id === id)
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+
+  app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    res.status(204).end()
+  })
+  
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
